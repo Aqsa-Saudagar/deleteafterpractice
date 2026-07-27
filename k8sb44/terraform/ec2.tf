850f260241bc6292c1d2e2a98bf00995a21226e9
Aqsa-Saudagar <aqsasaudagar145@gmail.com>
2026-07-27 21:39:39 +0530

    sa

---
+++ k8sb44/terraform/ec2.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+} 
+
+
+
+resource "aws_instance"  "webserver" {
+    ami = "ami-09d88f7c4c272b0c5" 
+    instance_type = "t3.micro"
+    vpc_security_group_ids =  ["sg-0d06a29eacde65701"]
+    key_name = "linuxkey"
+    tags = {
+        purpose = "webserver"
+    }
+    count = 2 
+}
