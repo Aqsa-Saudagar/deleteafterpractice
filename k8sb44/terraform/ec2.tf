93df7c2b44309f44e61647ad9aa2f89b954c2016
Aqsa-Saudagar <aqsasaudagar145@gmail.com>
2026-07-29 17:21:20 +0530

    sa

--- k8sb44/terraform/ec2.tf
+++ k8sb44/terraform/ec2.tf
@@ -15,4 +15,39 @@ resource "aws_instance"  "webserver" {
         purpose = "webserver"
     }
     count = 2 
+    user_data = <<-EOF
+                 #!/bin/bash
+                 sudo yum install nginx -y
+                 sudo systemctl start nginx
+
+
+
+                EOF
+}
+
+resource "aws_security_group" "webserver_sg" {
+    name = "tf-sg"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+
+    }
+
 }
