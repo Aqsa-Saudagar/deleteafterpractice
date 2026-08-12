cbe7dbe1ed8cdd6cc224df403aa77fe2c00feda6
Aqsa-Saudagar <aqsasaudagar145@gmail.com>
2026-08-12 14:05:39 +0530

    a

---
+++ k8sb44/terraform/provisioners/var.tf
@@ -0,0 +1,16 @@
+variable "amiid" {
+    default = "ami-0bdc7d025135d7b49"
+}
+
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+   default = "sg-084d391289b4b4019"
+}
+
+variable "kp" {
+    default = "key"
+
+}
